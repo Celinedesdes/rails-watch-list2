2c1eb96e1e310cf430d9e9f0f9395652fd8a1c7d
celine deshayes <deshayesceline@outlook.fr>
2026-06-14 17:34:15 +0200

    done

--- db/migrate/20260614151816_create_reviews.rb
+++ db/migrate/20260614151816_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[8.1]
   def change
     create_table :reviews do |t|
       t.text :content
-      t.string :author
+      t.integer :rating, null: false, default: 0
       t.references :list, null: false, foreign_key: true
 
       t.timestamps
--- db/migrate/20260614152426_change_reviews_remove_author_add_rating.rb
+++
@@ -1,6 +0,0 @@
-class ChangeReviewsRemoveAuthorAddRating < ActiveRecord::Migration[8.1]
-  def change
-    remove_column :reviews, :author, :string
-    add_column :reviews, :rating, :integer, null: false, default: 0
-  end
-end
